5d936853f61a0b76270d98adf566569f66dee7bd
m-c-hill <matthewhill2595@gmail.com>
2021-12-08 09:00:12 +0000

    assignment part 3 & 4

--- assignment_1.hs
+++ assignment_1.hs
@@ -91,44 +91,86 @@ used (Apply  m n) = merge (used m) (used n)  -- Term: MN (application)
 
 ------------------------- Assignment 3
 
--- Capture avoiding
-
+-- Renaming variables
 rename :: Var -> Var -> Term -> Term
-rename x y (Variable z) = undefined
-rename x y (Lambda z n) = undefined
-rename x y (Apply  n m) = undefined
+rename x y (Variable z)
+    | z == x = Variable y
+    | otherwise = Variable z
+rename x y (Lambda z m)
+    | z == x = Lambda z m
+    | otherwise = Lambda z (rename x y m)
+rename x y (Apply m n)
+    = Apply (rename x y m) (rename x y n)
 
-
+-- Capture avoiding substitution
 substitute :: Var -> Term -> Term -> Term
-substitute = undefined
+substitute x n (Variable y)
+    | y == x = n  -- This time, n is a variable
+    | otherwise = Variable y
+substitute x n (Lambda y m)
+    | y == x = Lambda y m
+    | otherwise = Lambda z (substitute x n (rename y z m))
+        where
+            z = fresh (merge [x] (merge (used n) (used m)))
+substitute x n (Apply m_1 m_2)
+    = Apply (substitute x n m_1) (substitute x n m_2)
 
 
 ------------------------- Assignment 4
 
+-- Beta reduction (normal order)
+
+-- Returns a list of all beta-reducts of a term.
+-- Following the beta-reduction logic detailed in the assignment, we apply beta to four pattern matching cases.
+-- This includes the 3 variable patterns used previously and a top-level beta-step (λx.N) M →β N[M/x]. Either term within a term can be beta reduced (defined below recursively).
 beta :: Term -> [Term]
-beta = undefined
+beta (Apply (Lambda x n) m) = [substitute x m n] ++ [Apply (Lambda x b) m  | b <- (beta n)] ++ [Apply (Lambda x n)  a | a <- (beta m)]
+beta (Apply n m) = [Apply b m  | b <- (beta n)] ++ [Apply n  a | a <- (beta m)]
+beta (Lambda x n) = [Lambda x b | b <- (beta n)]
+beta (Variable x) = []
 
+-- List of all beta-reduction steps which leads from the initial term to a final normal form
 normalize :: Term -> [Term]
-normalize = undefined
+normalize n
+    | null (beta n) = [n]
+    | otherwise = n : normalize (head (beta n))
 
+-- Return the normal form of term n by retrieving the last element of list produced by apply `normalize` to n.
+-- NOTE: Unsure how to tell if the list returned by normalize is infinite (Halting problem?)
+-- There may be infinite beta steps if the reduction is caught in a loop. In situations such as this, there will be no normal form.
 normal :: Term -> Term
-normal = undefined
+normal n = last (normalize n)
 
 -------------------------
+
+-- Beta reduction (applicative order)
+
+-- The above functions currently apply normal order beta reduction.
+-- In order to apply applicative order beta reduction, a_beta can be the same as beta, as this is simply finding all possible beta-reducts.
+-- However, the selection process (applied in a_normalize) will need to be updated.
 
 a_beta :: Term -> [Term]
-a_beta = undefined
+a_beta (Apply (Lambda x n) m) = [substitute x m n] ++ [Apply (Lambda x b) m  | b <- (a_beta n)] ++ [Apply (Lambda x n)  a | a <- (a_beta m)]
+a_beta (Apply n m) = [Apply b m  | b <- (a_beta n)] ++ [Apply n  a | a <- (a_beta m)]
+a_beta (Lambda x n) = [Lambda x b | b <- (a_beta n)]
+a_beta (Variable x) = []
 
+-- List of all beta-reduction steps which leads from the initial term to a final normal form
 a_normalize :: Term -> [Term]
-a_normalize = undefined
+a_normalize n
+    | null (beta n) = [n]
+    | otherwise = n : a_normalize (last (a_beta n))
 
 a_normal :: Term -> Term
-a_normal = undefined
+a_normal n = last (a_normalize n)
 
 -------------------------
 
+-- As seen in week 4's tutorial, the following term reduces with fewer steps using applicative-order reduction:
+-- (\f. \x. f (f x)) (\a. (\x. x) ((\y. y) ((\z. z) a)))
 example1 :: Term
-example1 = undefined
+example1 = Apply (Lambda "f" (Lambda "x" (Apply (Variable "f") (Apply (Variable"f") (Variable "x"))))) (Lambda "a" (Apply (Lambda "x" (Variable "x")) (Apply (Lambda "y" (Variable "y")) (Apply (Lambda "z" (Variable "z")) (Variable "a")))))
 
+-- The term (\x. \y. y) ((\x. x) (\x. x)) (\x. x) will reduce to normal form in fewer steps when applying normal-order reduction.
 example2 :: Term
-example2 = undefined
+example2 = (Apply (Apply (Lambda "x" (Lambda "y" (Variable "y"))) (Apply (Lambda "x" (Variable "x")) (Lambda "x" (Variable "x")))) (Lambda "x" (Variable "x")))
